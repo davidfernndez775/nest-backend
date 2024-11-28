55e67669ce5e5aafc9a327f505add5768c863cb7
davidfernndez775 <davidfernndez775@yahoo.com>
2024-11-28 12:18:56 -0500

    auth in progress

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+// creamos el formato de los documentos que se guardan en la base de datos
+@Schema()
+export class User {
+  // Mongo pone por defecto el campo id
+  // _id: string;
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ minlength: 6, required: true })
+  password: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+}
+
+// exportamos el esquema creado
+export const UserSchema = SchemaFactory.createForClass(User);
