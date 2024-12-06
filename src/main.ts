f513a4b6e644d91eaa757ca026262fa5b7db1233
davidfernndez775 <davidfernndez775@yahoo.com>
2024-12-06 09:38:02 -0500

    enable Cors fix for Postman

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  // el enableCors tiene que ir justo encima del await app.listen
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
